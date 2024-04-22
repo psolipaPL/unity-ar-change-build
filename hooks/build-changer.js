ccced1f756f4673e7f33174165db369d3b309388
ZZaRpp <neozarp@gmail.com>
2024-04-22 10:54:02 +0200

    Update build-changer.js

--- hooks/build-changer.js
+++ hooks/build-changer.js
@@ -1,13 +1,13 @@
 const utils = require("./ar-config");
 
 module.exports = function (context) {
-    const confs = utils.getConfigs();
-    const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathAndroid);
+    //const confs = utils.getConfigs();
+    //const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathAndroid);
 
     utils.logProperties();
 
     //Removal of unused resources
-    utils.removeUnusedFolders(context.opts.projectRoot, context.opts.projectRoot + confs.androidPath, appId, true);
+    //utils.removeUnusedFolders(context.opts.projectRoot, context.opts.projectRoot + confs.androidPath, appId, true);
 
-    utils.moveGSFiles(context.opts.projectRoot + confs.androidPath + appId + confs.firebaseSuffix +'/google-services.json', context.opts.projectRoot + confs.androidAppPath + 'google-services.json')
+    //utils.moveGSFiles(context.opts.projectRoot + confs.androidPath + appId + confs.firebaseSuffix +'/google-services.json', context.opts.projectRoot + confs.androidAppPath + 'google-services.json')
 }
