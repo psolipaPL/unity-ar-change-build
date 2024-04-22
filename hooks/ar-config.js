bc76811a42ba43840d0660ac83a54f8f12b766a6
ZZaRpp <neozarp@gmail.com>
2024-04-22 17:30:38 +0200

    Update ar-config.js

--- hooks/ar-config.js
+++ hooks/ar-config.js
@@ -145,7 +145,7 @@ function changeAppBuildGradle() {
     let path = "platforms/android/app/build.gradle";
     logFile(path);
     let strToFind = "dependencies {";
-    let replaceByStr = "dependencies {" + os.EOS + "implementation fileTree(dir: project(':unityLibrary').getProjectDir().toString() + ('\\\\libs'), include: ['*.jar'])" + os.EOL;
+    let replaceByStr = "dependencies { \\n implementation fileTree(dir: project(':unityLibrary').getProjectDir().toString() + ('\\\\libs'), include: ['*.jar'])" + os.EOL;
     changeFileContent(path,strToFind,replaceByStr);
     //Log the changed file
     logFile(path);
